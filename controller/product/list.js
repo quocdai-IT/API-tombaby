3e2bd07a64345345276d1f52f5228886408a27e6
quocdai-IT <nqdai2904@gmail.com>
2019-09-23 16:43:43 +0700

    add catalog on product

--- controller/product/list.js
+++ controller/product/list.js
@@ -1,5 +1,6 @@
-module.exports = function(req, res){
- 
+module.exports =async function(req, res){
+ try {
+    const _ = require('lodash')
     const con = require('../../connect');
     const Promise = require('bluebird')
     let numRows;
@@ -10,36 +11,46 @@ module.exports = function(req, res){
     const skip = page > 0? ((page-1) * numPerPage):0;
     const limit = skip + ',' + numPerPage;
 
+    let results =[];
     const queryAsync  = Promise.promisify(con.query.bind(con))
 
-   queryAsync("SELECT count(*) as numRows FROM mt_vi_page_products").then(results => {
-      numRows = results[0].numRows;
-      numPages = Math.ceil(numRows/numPerPage);
-      
-   })
-   .then(() => queryAsync(`SELECT * FROM mt_vi_page_products ORDER BY ID DESC LIMIT ${limit} `)).then((data) => {
-       let responsePayload = {
-           results: data
-       };
-       if(page < numPages){
-           responsePayload.pagination = {
-               totalNumberOfResults: numRows,
-               current: page,
-               perPage: numPerPage,
-               pervious: page > 0? page -1: undefined,
-               next: page < numPages -1 ?page+1:undefined
-           }
-       }
-       else{
-           responsePayload.pagination = {
-            err: 'queried page ' + page + ' is >= to maximum page number ' + numPages
-           }
-       }
-       res.success(responsePayload)
-   }).catch(err => {
-        res.json({error: err})
-   })
-  
-    
+    const count = await queryAsync("SELECT count(*) as numRows FROM mt_vi_page_products")
+    numRows = count[0].numRows;
+    numPages = Math.ceil(numRows/numPerPage);
+
+    const catalog = await  queryAsync("SELECT * FROM mt_vi_page_catalogs");
+    const data = await  queryAsync(`SELECT * FROM mt_vi_page_products ORDER BY ID DESC LIMIT ${limit} `)
+
+    data.forEach(product => {
+        catalog.forEach(cat => {
+            if(product.catid === cat.catid){
+                delete product.catid;
+                results.push({...product, catalog: cat})
+            }
+        })
+    })
+    let responsePayload = {
+        results: results
+    };
+    if(page < numPages){
+        responsePayload.pagination = {
+            totalNumberOfResults: numRows,
+            current: page,
+            perPage: numPerPage,
+            pervious: page > 0? page -1: undefined,
+            next: page < numPages -1 ?page+1:undefined
+        }
+    }
+    else{
+        responsePayload.pagination = {
+         err: 'queried page ' + page + ' is >= to maximum page number ' + numPages
+        }
+    }
+
+    res.success(responsePayload)
+ } catch (error) {
+     res.serverError(error)
+ }
+
   
 }
