0baedbb84356c06b74086e7f37db774de2d9b5ed
nqdung13 <nqdung13@gmail.com>
2019-10-31 15:49:02 +0700

    add samegroup

--- controller/get_samegroup/list.js
+++ controller/get_samegroup/list.js
@@ -1,30 +1,47 @@
-module.exports = function(req, res){
+module.exports = async function(req, res){
 	try{
 		const _ = require("lodash");
 		const con = require("../../connect");
 		const Promise = require("bluebird");	
 		const module_name = req.body.module_name;
+		const same_group = req.body.same_group;
 		const lang = req.query.lang;
 		const queryAsync = Promise.promisify(con.query.bind(con));	
 		let language = "vi";
-
 		if(lang){
-		  language  = lang
+			language  = lang
 		}
-		
 		let module_name_default = "page";
 		if(module_name){
 			module_name_default = module_name;
+		}	 		
+		let data = [];
+		const data_sum_num = [];
+		if(same_group){
+			data = await queryAsync(
+				`SELECT t1.* FROM mt_${language}_${module_name_default}_products AS t1 WHERE same_group = ${same_group} ORDER BY txt_properties ASC`
+			);
+			const module = await queryAsync(
+			  `SELECT * FROM mt_${language}_modules WHERE module_file = 'page' AND module_data != '${module_name_default}'`
+			);
+			const num_array = [];
+			for(i=0; i<data.length; i++){
+				let num = 0;
+				for(ii=0; ii<module.length; ii++){
+					const module_num = await queryAsync(
+					  `SELECT num FROM mt_${language}_${module[ii].module_data}_products WHERE id = ${data[i].id}`
+					);
+					num += module_num[0].num;
+				}
+				data[i].total_num = num;
+				data_sum_num[i]= data[i];
+			}
 		}
-		//const module = await queryAsync(
-		  //`SELECT * FROM mt_${language}_modules WHERE module_file = 'page' AND module_data != '${module_name_default}'`
-		//);
 		let responsePayload = {
-		  results: module_name_default
+		  results: data_sum_num
 		};		
 		res.success(responsePayload);
 	} catch (error) {
-		console.log(error);
 		res.serverError(error);
 	}
 };
--- controller/product/list.js
+++ controller/product/list.js
@@ -5,7 +5,7 @@ function panigation(page, numPages, numRows, numPerPage, response) {
       current: page,
       perPage: numPerPage,
       pervious: page > 0 ? page - 1 : undefined,
-      next: page < numPages - 1 ? page + 1 : undefined
+      next: page < numPages ? page + 1 : undefined
     };
   } else {
     response.pagination = {
@@ -26,16 +26,24 @@ module.exports = async function(req, res) {
     const page = parseInt(req.body.page) || 1;
     let numPages;
     const skip = page > 0 ? (page - 1) * numPerPage : 0;
-    const limit = " LIMIT "+skip + "," + numPerPage;
+    let limit = " LIMIT "+skip + "," + numPerPage;
     const id = req.body.id;
+    const ids = req.body.ids;
+    const no_limit = req.body.no_limit;
     const catid = req.body.catid;
     const list_cat = req.body.list_cat;
     const module_name = req.body.module_name;
+    const order_by = req.body.order_by;
+    const sort = req.body.sort;
     const lang = req.query.lang;
     const queryAsync = Promise.promisify(con.query.bind(con));
+    let distinct = "";
     let where = "WHERE t1.show_web = 1";	
     let language = "vi";
     let module_name_default = "page";
+    let order = "edit_time";
+    let groupby = "GROUP BY same_group ";
+    let order_type = "DESC";
     if (catid) {
       where += ` and t1.catid = ${catid}`;
     }
@@ -45,12 +53,26 @@ module.exports = async function(req, res) {
 	if (id) {
 		where += ` and t1.id = '${id}'`;		
 	}
+	if(ids){
+		where += ` and t1.id IN (${ids})`;
+		groupby = "";
+		distinct = "DISTINCT ";
+	}
     if(lang){
         language = lang;
     }
     if(module_name){
         module_name_default = module_name;
     }
+    if(order_by){
+        order = order_by;
+    }
+    if(sort){
+        order_type = sort;
+    }
+	if(no_limit){
+		limit = "";
+	}
     const count = await queryAsync(
       `SELECT count(DISTINCT(same_group)) as numRows FROM mt_${language}_${module_name_default}_products AS t1 ${where}`
     );
@@ -58,7 +80,7 @@ module.exports = async function(req, res) {
     numPages = Math.ceil(numRows / numPerPage);
 	where += ` AND t1.id = t2.pro_id`;
     const data = await queryAsync(
-      `SELECT t1.* FROM mt_${language}_${module_name_default}_products AS t1, mt_${language}_${module_name_default}_group_items AS t2 ${where} GROUP BY same_group ORDER BY ID DESC${limit}`
+      `SELECT ${distinct}t1.* FROM mt_${language}_${module_name_default}_products AS t1, mt_${language}_${module_name_default}_group_items AS t2 ${where} ${groupby}ORDER BY ${order} ${order_type}${limit}`
     );
 	const module = await queryAsync(
       `SELECT * FROM mt_${language}_modules WHERE module_file = 'page' AND module_data != '${module_name_default}'`
@@ -66,14 +88,21 @@ module.exports = async function(req, res) {
 	const num_array = [];
 	const data_sum_num = [];
 	for(i=0; i<data.length; i++){
+		let id = data[i].id;
 		let num = 0;
+		let same_num = 0;
 		for(ii=0; ii<module.length; ii++){
 			const module_num = await queryAsync(
 			  `SELECT num FROM mt_${language}_${module[ii].module_data}_products WHERE id = ${data[i].id}`
 			);
+			const module_same_num = await queryAsync(
+			  `SELECT sum(num) AS num FROM mt_${language}_${module[ii].module_data}_products WHERE same_group = ${data[i].same_group}`
+			);
 			num += module_num[0].num;
+			same_num += module_same_num[0].num;
 		}
 		data[i].total_num = num;
+		data[i].total_same_num = same_num;
 		data_sum_num[i]= data[i];
 	}
 	
